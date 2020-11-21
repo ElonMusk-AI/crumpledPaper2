8eb0da75915aed53a9538373e20e19fdd739d159
Sagnik <74058957+ElonMusk-AI@users.noreply.github.com>
2020-11-21 23:54:56 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,36 @@
+class paper {
+    constructor(x,y,r){
+        var option = {
+            "restitution": 0.3,
+            "friction": 0,
+            "density": 1
+        }
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.body = Bodies.circle(this.x,this.y,this.r,option);
+        this.image = loadImage("paper.png");
+    
+        World.add(world,this.body);
+    }
+    display(){
+        
+        fill("white");
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rectMode(CENTER);
+        ellipse(0,0,this.r,this.r);
+        imageMode(CENTER);
+        image(this.image,0,0,100,100);
+        pop();
+    }
+
+
+
+
+
+
+
+
+}
